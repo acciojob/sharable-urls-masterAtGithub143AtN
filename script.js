5cda14fe38b0903e23b784a9e64f38788a3c4f9c
Saket Suman <133560724+masterAtGithub143AtN@users.noreply.github.com>
2026-04-27 06:52:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+const name=document.getElementById("name")
+const year=document.getElementById("year")
+const submt=document.getElementById("button")
+const url=document.getElementById("url")
+
+submt.addEventListener('click', function() {
+	const name_n=name.value
+	const year_n=year.value
+	const new_url=`https://localhost:8080/?name=${name}`
+	if(year_n!=""){
+		new_url+=`&year=${year}`
+	}
+	url.innerText=new_url;
+})
